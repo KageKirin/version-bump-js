f4086b7d62dff8e2458ba5c10d32465e2b67768c
Chris Helmich <kagekirin@gmail.com>
2021-11-17 17:22:32 +0900

    Implement main functionality

--- version.js
+++ version.js
@@ -22,7 +22,7 @@ let subparsers = parser.add_subparsers({dest: 'action', help: 'action to execute
 subparsers.required = true
 
 parent_parser.add_argument('file',   { help: 'file to modify', type: String, default: "package.json"})
-parent_parser.add_argument('-r', '--regex', { help: 'ECMAScript Regular Expression to parse the version string', type: String, default: "" })
+parent_parser.add_argument('-r', '--regex', { help: 'ECMAScript Regular Expression to parse the version string', type: String, default: "^(?<major>[0-9]+)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)$" })
 
 const get_parser = subparsers.add_parser('get', { parents: [parent_parser], help: 'get version number'});
 const set_parser = subparsers.add_parser('set', { parents: [parent_parser], help: 'set version number'});
@@ -38,8 +38,8 @@ main();
 
 function main()
 {
-    console.dir(parser);
-    console.dir(args);
+    //console.dir(parser);
+    //console.dir(args);
     const fnMap = {
       "get": get_version,
       "set": set_version,
@@ -54,15 +54,73 @@ function main()
 
 function get_version()
 {
-  console.log('get');
+  let pkg = read_package_json(args.file);
+  let ver = pkg.version
+  let [major, minor, patch] = parse_version(ver);
+  console.log(`${major}.${minor}.${patch}`);
+  //console.dir({'major': major, 'minor': minor, 'patch': patch});
 }
 
 function set_version()
 {
-  console.log('set');
+  let verTpl = parse_version(args.version);
+  if (verTpl)
+  {
+    let [major, minor, patch] = verTpl;
+    let pkg = read_package_json(args.file);
+    pkg.version = args.version;
+    write_package_json(args.file, pkg);
+  }
+
+  return get_version();
 }
 
 function bump_version()
 {
-  console.log('bump');
+  let pkg = read_package_json(args.file);
+  let verTpl = parse_version(pkg.version);
+  if (verTpl)
+  {
+    let [major, minor, patch] = verTpl;
+
+    if (args.major)
+    {
+      major++;
+    }
+
+    if (args.minor)
+    {
+      minor++;
+    }
+
+    if (args.patch)
+    {
+      patch++;
+    }
+
+    pkg.version = `${major}.${minor}.${patch}`;
+    write_package_json(args.file, pkg);
+  }
+
+  return get_version();
+}
+
+function read_package_json(file)
+{
+  return JSON.parse(fs.readFileSync(file));
+}
+
+function write_package_json(file, data)
+{
+  fs.writeFileSync(file, JSON.stringify(data, null, '  ') + '\n');
+}
+
+function parse_version(version)
+{
+  let match = version.match(args.regex);
+  if (match)
+  {
+    return [match.groups.major, match.groups.minor, match.groups.patch];
+  }
+  return null
 }
