df5d77b561a5cdc231ec82f16b613e0d0b85f1ca
Chris Helmich <kagekirin@gmail.com>
2021-11-18 23:00:05 +0900

    Add semi-colons

--- version.js
+++ version.js
@@ -18,7 +18,7 @@ const parser = new ArgumentParser({
   parents: [parent_parser]
 });
 
-let subparsers = parser.add_subparsers({dest: 'action', help: 'action to execute'})
+let subparsers = parser.add_subparsers({dest: 'action', help: 'action to execute'});
 subparsers.required = true
 
 parent_parser.add_argument('file',   { help: 'file to modify', type: String, default: "package.json"})
@@ -26,12 +26,12 @@ parent_parser.add_argument('-r', '--regex', { help: 'ECMAScript Regular Expressi
 
 const get_parser = subparsers.add_parser('get', { parents: [parent_parser], help: 'get version number'});
 const set_parser = subparsers.add_parser('set', { parents: [parent_parser], help: 'set version number'});
-set_parser.add_argument('version',   { help: 'version to set', type: String})
+set_parser.add_argument('version',   { help: 'version to set', type: String});
 
 const bump_parser = subparsers.add_parser('bump', { parents: [parent_parser], help: 'bump version number'});
-bump_parser.add_argument('-M', '--major', { help: 'bump version major', action: 'store_true' })
-bump_parser.add_argument('-m', '--minor', { help: 'bump version minor', action: 'store_true' })
-bump_parser.add_argument('-p', '--patch', { help: 'bump version patch', action: 'store_true' })
+bump_parser.add_argument('-M', '--major', { help: 'bump version major', action: 'store_true' });
+bump_parser.add_argument('-m', '--minor', { help: 'bump version minor', action: 'store_true' });
+bump_parser.add_argument('-p', '--patch', { help: 'bump version patch', action: 'store_true' });
 
 const args = parser.parse_args();
 main();
